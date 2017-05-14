72d527aa0d107edbe9b75313ca68f117ed25fa1a
sagarafr <sagarafr@gmail.com>
2017-05-14 16:30:22 +0200

    feat(main): Add Message decorator feature

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from telegram.ext import Updater
 from commands import Command
+from commands import Message
 
 
 def main():
     command = Command()
+    message = Message()
     updater = Updater(input("Token: "))
     command.init_dispatcher(updater)
+    message.init_dispatcher(updater)
     updater.start_polling()
     updater.idle()
 
