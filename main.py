c4baa286a271d6898cd7adc6748909339507a3a0
sagarafr <sagarafr@gmail.com>
2017-05-14 18:54:34 +0200

    feat(main): Add BasicConfiguration in main

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from telegram.ext import Updater
 from commands import Command
 from commands import Message
+from configuration.basic_configuration import BasicConfiguration
 
 
 def main():
     command = Command()
     message = Message()
-    updater = Updater(input("Token: "))
+    config = BasicConfiguration('./resources/configuration.ini')
+    updater = Updater(config.token)
     command.init_dispatcher(updater)
     message.init_dispatcher(updater)
     updater.start_polling()
