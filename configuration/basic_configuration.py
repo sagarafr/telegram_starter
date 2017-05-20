d773a409227bfdb5899d86c2fae240bd8fcb3b96
sagarafr <sagarafr@gmail.com>
2017-05-21 00:04:55 +0200

    feat(config): Add trust_admin_in_chan field This field is a boolean that say if the administrators in chan is trusted

--- configuration/basic_configuration.py
+++ configuration/basic_configuration.py
@@ -6,7 +6,13 @@ class BasicConfiguration(object):
         self._parser = ConfigParser()
         with open(file, 'r') as fd_file:
             self._parser.read_file(fd_file)
-            self._admin = None if not self._parser.has_option('DEFAULT', 'admins') else [int(e) for e in self._parser['DEFAULT']['admins'].split(';')]
+            self._admin = None if not self._parser.has_option('DEFAULT', 'admins') else [int(e) for e in self._parser['DEFAULT']['admins'].split(';') if str(e).isdigit()]
+            self._trust_admin_in_chan = None
+            if not self._parser.has_option('DEFAULT', 'trust_admin_in_chan'):
+                try:
+                    self._trust_admin_in_chan = self._parser.getboolean('DEFAULT', 'trust_admin_in_chan')
+                except ValueError:
+                    self._trust_admin_in_chan = None
 
     @property
     def token(self):
@@ -15,3 +21,7 @@ class BasicConfiguration(object):
     @property
     def admins(self):
         return self._admin
+
+    @property
+    def trust_admin_in_chan(self):
+        return self._trust_admin_in_chan
