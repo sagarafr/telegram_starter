2f78cf24c70609675f3a09ac7c45cc60ba255150
sagarafr <sagarafr@gmail.com>
2017-07-02 23:36:42 +0200

    chore(main): Add absolut path to the configuration

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+from os import getcwd
+from os.path import join as path_join
 from configuration.basic_configuration import BasicConfiguration
 from admin.admin import admin
 
-config_file = BasicConfiguration('./resources/configuration.ini')
+config_file = BasicConfiguration(path_join(getcwd(), './resources/configuration.ini'))
 admin_grp = admin(config_file.admins)
 
 
